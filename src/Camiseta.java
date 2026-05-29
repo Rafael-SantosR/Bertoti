55b3f55dc2522e126114d662d993e07707b6faf5
Rafael-SantosR <rafael.spsjcpt@gmail.com>
2026-05-29 19:28:49 -0300

    feat: Adição da atividade 6

---
+++ src/Camiseta.java
@@ -0,0 +1,37 @@
+public class Camiseta {
+
+    private String marca;
+    private String cor;
+    private String tamanho;
+
+    public Camiseta(String marca, String cor, String tamanho) {
+        this.marca = marca;
+        this.cor = cor;
+        this.tamanho = tamanho;
+    }
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+}
